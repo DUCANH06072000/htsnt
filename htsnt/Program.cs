ae250cc95f34f229264362efa36a8dac8520ac67
anh duc <bosin233@gmail.com>
2020-10-08 00:05:33 +0700

    Add project files.

---
+++ htsnt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace htsnt
+{
+    class Program
+    {
+        #region htnt
+        static bool kt(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("nhap so n:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+                if (kt(i))
+                {
+                    Console.WriteLine("noi dung hien thi:{0}", i);
+
+
+                }
+            #endregion
+
+
+
+        }
+}
+}
